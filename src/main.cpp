441eca04f8c6869a4c95428e5c43d2876a457c53
Dongha Chung <haetae.dongha@gmail.com>
2021-08-31 16:19:59 +0900

    still setting up;

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <Eigen/Core>
+#include <ros/ros.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/geometry/Pose3.h>
 
 int main(int argc, char ** argv) {
 
-
+    Eigen::Matrix3f A;
+    A<<1, 2, 3, 4, 5, 6, 7, 8, 9;
+    std::cout<<A<<std::endl;
     return 0;
 }
