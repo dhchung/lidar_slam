b605d1bb56ec6dfeba2f7ba7fa70b80c86346cbc
Dongha Chung <haetae.dongha@gmail.com>
2021-09-03 15:10:35 +0900

    optim

---
+++ include/lidar_slam/optimizer.h
@@ -0,0 +1,18 @@
+#ifndef OPTIMIZER_H
+#define OPTIMIZER_H
+#include <queue>
+
+
+struct DataHandle{
+    
+}
+
+
+class Optimizer{
+public:
+    Optimizer();
+    ~Optimizer();
+};
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,23 @@
 #include <ros/ros.h>
 #include <core_msgs/string_w_header.h>
 #include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
 #include <tf/tf.h>
 
 #include "gps_module.h"
+#include "data_handle.h"
+
 
 GPSModule gpsmodule;
+DataHandle <sensor_msgs::Imu> imu_data;
+DataHandle <nav_msgs::Odometry> gps_data;
 
 void cb_gps(core_msgs::string_w_header::ConstPtr msg) {
     core_msgs::string_w_header data = *msg;
     bool gps_data = gpsmodule.cb_gps(data);
     if(gps_data) {
-    }
 
+    }
 }
 
 
